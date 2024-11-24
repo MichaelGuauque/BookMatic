183c8934838688bc28d08254c03380ceb3c45bc7
MichaelGuauque <mcalejo98@gmail.com>
2024-11-24 11:13:00 -0500

    Carga proyecto

---
+++ src/main/java/com/bookmatic/BookMaticApplication.java
@@ -0,0 +1,13 @@
+package com.bookmatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMaticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMaticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookmatic/BookMaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMaticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
